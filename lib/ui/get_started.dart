e4eae6c83f794827e3f9de2a7449a90c0238b23e
YouMe6110 <ume0116@naver.com>
2022-09-07 21:44:33 +0900

    Flutter UI

--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -5,10 +5,34 @@ class GetStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather App'),
+        body: Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.red,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/get-started.png'),
+            const SizedBox(height: 30,),
+            Container(
+              height: 50,
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: const BorderRadius.all(Radius.circular(10),),
+              ),
+              child: const Center(
+                child: Text('Get started', style: TextStyle(color: Colors.white, fontSize: 18),),
+              ),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
