ff6c973f46f7d10cabca9672abd4ae1fae21be54
YouMe6110 <ume0116@naver.com>
2022-09-09 10:00:44 +0900

    Flutter UI

--- lib/ui/welcome.dart
+++ lib/ui/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather/models/city.dart';
 import 'package:weather/models/constants.dart';
+import 'package:weather/ui/home.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({Key? key}) : super(key: key);
@@ -12,7 +13,8 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    List<City> cities = City.citiesList.where((city) => city.isDefault == false).toList();
+    List<City> cities =
+        City.citiesList.where((city) => city.isDefault == false).toList();
     List<City> selectedCities = City.getSelectedCities();
 
     Constants myConstants = Constants();
@@ -27,17 +29,19 @@ class _WelcomeState extends State<Welcome> {
       body: ListView.builder(
         physics: const BouncingScrollPhysics(),
         itemCount: cities.length,
-        itemBuilder: (BuildContext context, int index){
+        itemBuilder: (BuildContext context, int index) {
           return Container(
             margin: const EdgeInsets.only(left: 10, top: 20, right: 10),
             padding: const EdgeInsets.symmetric(horizontal: 20),
             height: size.height * .08,
             width: size.width,
             decoration: BoxDecoration(
-                border: cities[index].isSelected == true ? Border.all(
-                  color: myConstants.secondaryColor.withOpacity(.6),
-                  width: 2,
-                ) : Border.all(color: Colors.white),
+                border: cities[index].isSelected == true
+                    ? Border.all(
+                        color: myConstants.secondaryColor.withOpacity(.6),
+                        width: 2,
+                      )
+                    : Border.all(color: Colors.white),
                 borderRadius: const BorderRadius.all(Radius.circular(10)),
                 boxShadow: [
                   BoxShadow(
@@ -46,25 +50,44 @@ class _WelcomeState extends State<Welcome> {
                     blurRadius: 7,
                     offset: const Offset(0, 3),
                   )
-                ]
-            ),
+                ]),
             child: Row(
               children: [
                 GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       setState(() {
-                        cities[index].isSelected =! cities[index].isSelected;
+                        cities[index].isSelected = !cities[index].isSelected;
                       });
                     },
-                    child: Image.asset(cities[index].isSelected == true ? 'assets/checked.png' : 'assets/unchecked.png', width: 30,)),
-                const SizedBox( width: 10,),
-                Text(cities[index].city, style: TextStyle(
-                  fontSize: 16,
-                  color: cities[index].isSelected == true ? myConstants.primaryColor : Colors.black54,
-                ),)
+                    child: Image.asset(
+                      cities[index].isSelected == true
+                          ? 'assets/checked.png'
+                          : 'assets/unchecked.png',
+                      width: 30,
+                    )),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  cities[index].city,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: cities[index].isSelected == true
+                        ? myConstants.primaryColor
+                        : Colors.black54,
+                  ),
+                )
               ],
             ),
           );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: myConstants.secondaryColor,
+        child: const Icon(Icons.pin_drop),
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Home()));
         },
       ),
     );
